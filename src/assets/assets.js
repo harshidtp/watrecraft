fc6e8c083e33f514cec2411219a8e69dca8b78ea
user <harshidtp@gmail.com>
2025-08-30 19:10:43 +0300

    Fix: removed group_profile reference

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,40 +1,38 @@
-import logo from './logo.png'
-import arrow_icon from './arrow_icon.svg'
-import group_profile from './group_profile.png'
-import bgImage1 from './bgImage1.png'
-import bgImage2 from './bgImage2.png'
-import hero_img from './hero_img.png'
-import microsoft_logo from './microsoft_logo.png'
-import zoom_logo from './zoom_logo.png'
-import rakuten_logo from './rakuten_logo.png'
-import coinbase_logo from './coinbase_logo.png'
-import ads_icon from './ads_icon.svg'
-import content_icon from './content_icon.svg'
-import marketing_icon from './marketing_icon.svg'
-import social_icon from './social_icon.svg'
-import menu_icon from './menu_icon.svg'
-import close_icon from './close_icon.svg'
-import work_mobile_app from './work_mobile_app.png'
-import work_fitness_app from './work_fitness_app.png'
-import work_dashboard_management from './work_dashboard_management.png'
-import email_icon from './email_icon.svg'
-import person_icon from './person_icon.svg'
-import facebook_icon from './facebook_icon.svg'
-import twitter_icon from './twitter_icon.svg'
-import instagram_icon from './instagram_icon.svg'
-import linkedin_icon from './linkedin_icon.svg'
-import logo_dark from './logo_dark.png'
-import airbnb_logo from './airbnb_logo.svg'
-import google_logo from './google_logo.svg'
-import menu_icon_dark from './menu_icon_dark.svg'
-import sun_icon from './sun_icon.svg'
-import moon_icon from './moon_icon.svg'
-import about_img from './about.jpg'
-import bottle_img1 from './300l_bottle.png'
-import bottle_img2 from './1l_bottle.png'
-import bottle_img3 from './20l_bottle.png'
-import footer_icon from './footer_logo.png'
-
+import logo from './logo.png';
+import arrow_icon from './arrow_icon.svg';
+import bgImage1 from './bgImage1.png';
+import bgImage2 from './bgImage2.png';
+import hero_img from './hero_img.png';
+import microsoft_logo from './microsoft_logo.png';
+import zoom_logo from './zoom_logo.png';
+import rakuten_logo from './rakuten_logo.png';
+import coinbase_logo from './coinbase_logo.png';
+import ads_icon from './ads_icon.svg';
+import content_icon from './content_icon.svg';
+import marketing_icon from './marketing_icon.svg';
+import social_icon from './social_icon.svg';
+import menu_icon from './menu_icon.svg';
+import close_icon from './close_icon.svg';
+import work_mobile_app from './work_mobile_app.png';
+import work_fitness_app from './work_fitness_app.png';
+import work_dashboard_management from './work_dashboard_management.png';
+import email_icon from './email_icon.svg';
+import person_icon from './person_icon.svg';
+import facebook_icon from './facebook_icon.svg';
+import twitter_icon from './twitter_icon.svg';
+import instagram_icon from './instagram_icon.svg';
+import linkedin_icon from './linkedin_icon.svg';
+import logo_dark from './logo_dark.png';
+import airbnb_logo from './airbnb_logo.svg';
+import google_logo from './google_logo.svg';
+import menu_icon_dark from './menu_icon_dark.svg';
+import sun_icon from './sun_icon.svg';
+import moon_icon from './moon_icon.svg';
+import about_img from './about.jpg';
+import bottle_img1 from './300l_bottle.png';
+import bottle_img2 from './1l_bottle.png';
+import bottle_img3 from './20l_bottle.png';
+import footer_icon from './footer_logo.png';
 
 export const company_logos = [
   microsoft_logo,
@@ -43,12 +41,11 @@ export const company_logos = [
   coinbase_logo,
   airbnb_logo,
   google_logo,
-]
+];
 
 const assets = {
   logo,
   arrow_icon,
-  group_profile,
   bgImage1,
   bgImage2,
   hero_img,
@@ -75,10 +72,10 @@ const assets = {
   bottle_img1,
   bottle_img2,
   bottle_img3,
-  footer_icon
-}
+  footer_icon,
+};
 
-export default assets
+export default assets;
 
 export const teamData = [
   { name: 'Haley Carter', title: 'CEO & founder', image: 'https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=200' },
@@ -89,4 +86,4 @@ export const teamData = [
   { name: 'Ryan Mitchell', title: 'Content writer', image: 'https://randomuser.me/api/portraits/men/9.jpg' },
   { name: 'Megan Brooks', title: 'Performance manager', image: 'https://randomuser.me/api/portraits/women/12.jpg' },
   { name: 'Amber Foster', title: 'Senior writer', image: 'https://randomuser.me/api/portraits/women/14.jpg' },
-]
+];
