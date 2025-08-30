b58e59bb6a1152ec9d5a456abef970924950a071
user <harshidtp@gmail.com>
2025-08-30 19:12:50 +0300

    Remove company logos to fix missing file errors

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,12 +1,9 @@
+// src/assets/assets.js
 import logo from './logo.png';
 import arrow_icon from './arrow_icon.svg';
 import bgImage1 from './bgImage1.png';
 import bgImage2 from './bgImage2.png';
 import hero_img from './hero_img.png';
-import microsoft_logo from './microsoft_logo.png';
-import zoom_logo from './zoom_logo.png';
-import rakuten_logo from './rakuten_logo.png';
-import coinbase_logo from './coinbase_logo.png';
 import ads_icon from './ads_icon.svg';
 import content_icon from './content_icon.svg';
 import marketing_icon from './marketing_icon.svg';
@@ -23,8 +20,6 @@ import twitter_icon from './twitter_icon.svg';
 import instagram_icon from './instagram_icon.svg';
 import linkedin_icon from './linkedin_icon.svg';
 import logo_dark from './logo_dark.png';
-import airbnb_logo from './airbnb_logo.svg';
-import google_logo from './google_logo.svg';
 import menu_icon_dark from './menu_icon_dark.svg';
 import sun_icon from './sun_icon.svg';
 import moon_icon from './moon_icon.svg';
@@ -33,15 +28,6 @@ import bottle_img1 from './300l_bottle.png';
 import bottle_img2 from './1l_bottle.png';
 import bottle_img3 from './20l_bottle.png';
 import footer_icon from './footer_logo.png';
-
-export const company_logos = [
-  microsoft_logo,
-  zoom_logo,
-  rakuten_logo,
-  coinbase_logo,
-  airbnb_logo,
-  google_logo,
-];
 
 const assets = {
   logo,
@@ -76,14 +62,3 @@ const assets = {
 };
 
 export default assets;
-
-export const teamData = [
-  { name: 'Haley Carter', title: 'CEO & founder', image: 'https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=200' },
-  { name: 'James Walker', title: 'Ads manager', image: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=200' },
-  { name: 'Jessica Morgan', title: 'Vice president', image: 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=200&h=200&auto=format&fit=crop' },
-  { name: 'Ashley Bennett', title: 'Marketing & sales', image: 'https://randomuser.me/api/portraits/women/10.jpg' },
-  { name: 'Emily Parker', title: 'Content marketer', image: 'https://randomuser.me/api/portraits/women/11.jpg' },
-  { name: 'Ryan Mitchell', title: 'Content writer', image: 'https://randomuser.me/api/portraits/men/9.jpg' },
-  { name: 'Megan Brooks', title: 'Performance manager', image: 'https://randomuser.me/api/portraits/women/12.jpg' },
-  { name: 'Amber Foster', title: 'Senior writer', image: 'https://randomuser.me/api/portraits/women/14.jpg' },
-];
