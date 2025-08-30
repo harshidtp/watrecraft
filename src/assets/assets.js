17987d97aec3a5318fb567dbb93b9caf2d6e6d7b
user <harshidtp@gmail.com>
2025-08-30 19:06:31 +0300

    Fix TrustedBy: remove company_logos import

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,17 +1,31 @@
 import logo from './logo.png'
 import arrow_icon from './arrow_icon.svg'
-
+import group_profile from './group_profile.png'
 import bgImage1 from './bgImage1.png'
 import bgImage2 from './bgImage2.png'
 import hero_img from './hero_img.png'
-
-
-
+import microsoft_logo from './microsoft_logo.png'
+import zoom_logo from './zoom_logo.png'
+import rakuten_logo from './rakuten_logo.png'
+import coinbase_logo from './coinbase_logo.png'
+import ads_icon from './ads_icon.svg'
+import content_icon from './content_icon.svg'
+import marketing_icon from './marketing_icon.svg'
+import social_icon from './social_icon.svg'
 import menu_icon from './menu_icon.svg'
 import close_icon from './close_icon.svg'
-
+import work_mobile_app from './work_mobile_app.png'
+import work_fitness_app from './work_fitness_app.png'
+import work_dashboard_management from './work_dashboard_management.png'
+import email_icon from './email_icon.svg'
+import person_icon from './person_icon.svg'
+import facebook_icon from './facebook_icon.svg'
+import twitter_icon from './twitter_icon.svg'
+import instagram_icon from './instagram_icon.svg'
+import linkedin_icon from './linkedin_icon.svg'
 import logo_dark from './logo_dark.png'
-
+import airbnb_logo from './airbnb_logo.svg'
+import google_logo from './google_logo.svg'
 import menu_icon_dark from './menu_icon_dark.svg'
 import sun_icon from './sun_icon.svg'
 import moon_icon from './moon_icon.svg'
@@ -22,6 +36,14 @@ import bottle_img3 from './20l_bottle.png'
 import footer_icon from './footer_logo.png'
 
 
+export const company_logos = [
+  microsoft_logo,
+  zoom_logo,
+  rakuten_logo,
+  coinbase_logo,
+  airbnb_logo,
+  google_logo,
+]
 
 const assets = {
   logo,
--- src/components/TrustedBy.jsx
+++ src/components/TrustedBy.jsx
@@ -1,17 +1,30 @@
-import React from 'react'
-import {company_logos} from '../assets/assets'
-function TrustedBy() {
-  return (
-    <div className='flex flex-col items-center px-4 sm:px-12 lg:px-24 xl:px-40 gap-10 text-gray-700 dark:text-white/80'>
-     <h3 className='font-semibold'>Trusted by Leading Companies</h3>
+import React from 'react';
+import assets from '../assets/assets';
 
-     <div className='flex items-center flex-wrap gap-10 m-4'>
-      {company_logos.map((logo, index)=>(
-        <img key={index} src={logo} alt="" className='max-h-5 sm:max-h-6 dark:drop-shadow-xl'/>
-      ))}
-     </div>
+function TrustedBy() {
+  // pick which logos you actually want to show
+  const companyLogos = [
+    assets.logo,
+    assets.logo_dark,
+    assets.footer_icon,
+  ].filter(Boolean); // filters out undefined
+
+  return (
+    <div className="flex flex-col items-center px-4 sm:px-12 lg:px-24 xl:px-40 gap-10 text-gray-700 dark:text-white/80">
+      <h3 className="font-semibold">Trusted by Leading Companies</h3>
+
+      <div className="flex items-center flex-wrap gap-10 m-4">
+        {companyLogos.map((logo, index) => (
+          <img
+            key={index}
+            src={logo}
+            alt={`Company logo ${index + 1}`}
+            className="max-h-5 sm:max-h-6 dark:drop-shadow-xl"
+          />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default TrustedBy
+export default TrustedBy;
